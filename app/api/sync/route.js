5d717dde9e54fb105ceb1b003601042aea4c9a4a
deepafx26 <deepafx26@gmail.com>
2026-03-05 16:10:44 +0700

    add trader auto insert 23

--- app/api/sync/route.js
+++ app/api/sync/route.js
@@ -5,66 +5,38 @@ const supabase = createClient(
  process.env.SUPABASE_SERVICE_ROLE_KEY
 )
 
-const email = process.env.MYFXBOOK_EMAIL
-const password = process.env.MYFXBOOK_PASSWORD
-    
 export async function GET() {
 
- const login = await fetch(
-  `https://www.myfxbook.com/api/login.json?email=${email}&password=${password}`
- )
-if (!email || !password) {
-        return new Response("Missing Myfxbook credentials")
-    }
- const loginData = await login.json()
- const session = loginData.session
+ try {
 
- const accountsRes = await fetch(
-  `https://www.myfxbook.com/api/get-my-accounts.json?session=${session}`
- )
+  const email = process.env.MYFXBOOK_EMAIL
+  const password = process.env.MYFXBOOK_PASSWORD
 
- const accounts = await accountsRes.json()
+  const login = await fetch(
+   `https://www.myfxbook.com/api/login.json?email=${email}&password=${password}`
+  )
 
- for (const account of accounts.accounts) {
+  const loginData = await login.json()
+  const session = loginData.session
 
-  const accountId = account.id
+  const accountsRes = await fetch(
+   `https://www.myfxbook.com/api/get-my-accounts.json?session=${session}`
+  )
 
-  const { data: traders } = await supabase
-    .from("traders")
-    .select("*")
-    .eq("myfxbook_account_id", accountId)
+  const accounts = await accountsRes.json()
 
-  if (!traders || traders.length === 0) {
+  console.log("ACCOUNTS:", accounts)
 
-    await supabase
-      .from("traders")
-      .insert({
-        myfxbook_account_id: accountId,
-        name: account.name,
-        current_equity: account.equity,
-        growth_percentage: account.gain,
-        drawdown_percentage: account.drawdown,
-        created_at: new Date()
-      })
+  return Response.json(accounts)
 
-  } else {
+ } catch (error) {
 
-    const trader = traders[0]
+  console.error("SYNC ERROR:", error)
 
-    await supabase
-      .from("traders")
-      .update({
-        current_equity: account.equity,
-        growth_percentage: account.gain,
-        drawdown_percentage: account.drawdown,
-        updated_at: new Date()
-      })
-      .eq("id", trader.id)
+  return Response.json({
+   error: error.message
+  })
 
-  }
-
-}
-
- return Response.json({ status: "sync complete" })
+ }
 
 }
